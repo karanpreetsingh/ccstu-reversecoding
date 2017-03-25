f4c6131201a05c57ac37c48242f4c8b02c327e8c
Karanpreet Singh <mailtokaranpreet@gmail.com>
2017-03-25 11:36:35 +0530

    Remove old code

--- codes/contratakers.cpp
+++
@@ -1,6 +0,0 @@
-//how to take input and make matrix
-/*runs in float ,not in double*/
-#include<iostream>
-using namespace std;int main()//haveefunasdgasdgfsdgh
-{//thapar university
- double n,a,b;cin>>n;cin>>a;cin>>b;/*asbfadfgsd*/  for (int i=1;i<=n;i++)/*thapar*/{double c=(a+(i*(b-a))/(n+1));cout<<c;cout <<'\n';  }  return 0;    }
